f4eef52ecf84a674c8eea8868e3a083f98fc1e9a
Eberpraw <toby@easy.poker>
2024-06-25 15:57:50 +0200

    Initial Commit

---
+++ Sam/SamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamApp.swift
+//  Sam
+//
+//  Created by Tobias Eberhard on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
